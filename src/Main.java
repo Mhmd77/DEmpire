1d75eb589aa884755aeb67133c2f2853318d8ebd
md._.te <m.emami9@yahoo.com>
2017-06-22 02:16:56 +0430

    Zoom in Zoom out Ok Shod Monde Chapo Rast ba Mouse

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.Socket;
 
 import javafx.scene.input.MouseEvent;
-import javafx.util.Duration;
 
 public class Main extends Application {
     private static final int WIDTH = 1280;
@@ -24,6 +23,7 @@ public class Main extends Application {
     private static ServerListener serverListener;
     private static Game game;
     EventHandler<MouseEvent> mouseMove;
+    private final double SCALE_DELTA = 1.05;
 
     public static void main(String[] args) throws IOException {
         game = new Game();
@@ -53,32 +53,46 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        ScrollPane sp = new ScrollPane(root);
-        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        Scene scene = new Scene(sp);
-        Canvas canvas = new Canvas(WIDTH, HEIGHT);
-        OST ost = new OST();
-        OST battle = new OST();
-        OST menu = new OST();
-        Graphic graphic = new Graphic(root, scene, canvas, primaryStage);
-        ost.playSountrack();
-        graphic.setGraphics();
-        root.setOnMouseMoved(new EventHandler<MouseEvent>() {
+        VBox root = new VBox();
+        primaryStage.setTitle("TilePane Experiment");
+        GridPane grid = new GridPane();
+        MapLoader mapLoader = new MapLoader();
+        mapLoader.drawWorld(grid);
+        ScrollPane sp = new ScrollPane(grid);
+        grid.setOnScroll(new EventHandler<ScrollEvent>() {
             @Override
-            public void handle(MouseEvent event) {
-                if (event.getX() > scene.getWidth() - 20)
-                    sp.setHvalue(sp.getHvalue() + 0.005);
-                else if (event.getX() < scene.getWidth() + 20)
-                    sp.setHvalue(sp.getHvalue() - 0.005);
-                if (event.getY() > scene.getHeight() - 20)
-                    sp.setVvalue(sp.getVvalue() + 0.005);
-                else if (event.getY() < scene.getHeight() + 20)
-                    sp.setVvalue(sp.getVvalue() - 0.005);
-                System.out.println(event.getX());
+            public void handle(ScrollEvent event) {
+//                event.consume();
+
+                if (event.getDeltaY() == 0) {
+                    return;
+                }
+
+                double scaleFactor =
+                        (event.getDeltaY() > 0)
+                                ? SCALE_DELTA
+                                : 1 / SCALE_DELTA;
+                if(grid.getScaleX() < 0.65 && event.getDeltaY() < 0);
+                else if(grid.getScaleX() > 1.1 && event.getDeltaY() > 0);
+                else  {
+
+                    grid.setScaleX(grid.getScaleX() * scaleFactor);
+                    grid.setScaleY(grid.getScaleY() * scaleFactor);
+                }
             }
         });
+        grid.setOnMousePressed(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() == 2) {
+                    grid.setScaleX(1.0);
+                    grid.setScaleY(1.0);
+                }
+            }
+        });
+        root.getChildren().add(sp);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 
